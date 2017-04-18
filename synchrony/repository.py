a7733c7e30597a475fbd7486401f0d316cd93613
Gordon Finnie III <gordon.m.finnie.iii@gmail.com>
2017-04-18 15:52:42 -0400

    work on repo class, testcase wrapper

--- synchrony/repository.py
+++ synchrony/repository.py
@@ -1,4 +1,15 @@
 
 class Repository(object): 
 
-    def __init__(self):
+    def __init__(self, source):
+        
+        self._source = source
+
+    def get_source(): 
+        
+        return self._source
+        
+    def get_file():
+
+    def get_dir(): 
+
